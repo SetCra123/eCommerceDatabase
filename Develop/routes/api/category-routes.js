24b7d3a3ff2b083dde71bec0b8d718c4248ad8dd
SetrigeCrawford <setrige.crawford@journalism.cuny.edu>
2024-08-06 23:37:41 -0400

    I completed all routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,26 +3,84 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      // be sure to include its associated Products
+      include: [{ model: Product}]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      // be sure to include its associated Products
+      include: [{ model: Product }]
+    });
+    if(!categoryData) {
+      res.status(404).json({ message: 'No category found with that iD!' });
+      return; 
+    }
+    res.status(200).json(categoryData);
+   } catch (err) {
+      res.status(500).json(err);
+  }
+  
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    categoryId: req.body.categoryID,
+    categoryName: req.body.categoryName,
+  })
+  .then((newCategory) => {
+    
+    res.json(newCategory);
+    })
+    .catch((err) => {
+    res.json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      categoryId: req.body.categoryID,
+      categoryName: req.body.categoryName,
+    },
+    {
+      where: {
+        isbn: req.params.isbn,
+      },
+    }
+  )
+  .then((updatedCategory) => {
+    res.json(updatedCategory);
+    })
+  .catch((err) => res.json(err)); 
+  
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+    id: req.params.id,
+    },
+  })
+  .then((deletedCategory) => {
+    res.json(deletedCategory);
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,26 +3,93 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
-  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findAll({
+      // be sure to include its associated Product data
+      include: [{ model: Product }]
+    });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+  
 });
 
-router.get('/:id', (req, res) => {
+
+
+
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
-  // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      // be sure to include its associated Product data
+      include: [{ model: Product }]
+    });
+    if(!tagData) {
+      res.status(404).json({ message: 'No tag found with that iD!' });
+      return; 
+    }
+    res.status(200).json(driverData);
+   } catch (err) {
+      res.status(500).json(err);
+   }
+  
 });
+
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tagID: req.body.tagID,
+    tagName: req.body.tagName,
+  })
+  .then((newTag) => {
+    
+  res.json(newTag);
+  })
+  .catch((err) => {
+  res.json(err);
+  });
 });
+
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+    {
+      tagId: req.body.tagID,
+      tagName: req.body.tagName,
+    },
+    {
+      where: {
+        isbn: req.params.isbn,
+      },
+    }
+  )
+  .then((updatedBook) => {
+    res.json(updatedBook);
+    })
+  .catch((err) => res.json(err));
 });
+
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+    id: req.params.id,
+    },
+  })
+  .then((deletedTag) => {
+    res.json(deletedTag);
+    })
+    .catch((err) => res.json(err));
 });
 
+
+
 module.exports = router;
+
+
